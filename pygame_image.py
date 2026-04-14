e339e58e2f12324c87d7f5f872646f8e077f55c0
kazuki <c0c240600f@edu.teu.ac.jp>
2026-04-14 16:08:41 +0900

    練習７，８，９

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,21 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習１
+    bg_img2 = pg.transform.flip(bg_img, True, False) #練習８
     kk_img = pg.image.load("fig/3.png") #練習３
     kk_img = pg.transform.flip(kk_img, True, False)
 
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習２
         x = tmr #練習５
-        screen.blit(bg_img, [-x, 0])
-
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0]) #練習２
+        screen.blit(bg_img2, [-x+1600, 0])#練習７
+        screen.blit(bg_img, [-x+3200, 0]) #練習９
         screen.blit(kk_img, [300, 200]) #練習４
         pg.display.update()
         tmr += 1        
